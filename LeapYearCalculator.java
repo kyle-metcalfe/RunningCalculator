d9cfd6ad820a361bf593bbc25cd83ec5f5931fcd
kyle-metcalfe <166643859+kyle-metcalfe@users.noreply.github.com>
2024-06-26 15:12:16 -0400

    Update LeapYearCalculator.java

--- LeapYearCalculator.java
+++ LeapYearCalculator.java
@@ -2,16 +2,12 @@ package com.codewithkyle;
 
 public class LeapYearCalculator {
   public static boolean isLeapYear(int year) {
-    // Flag to take a non-leap year by default
     boolean is_leap_year = false;
 
-    // If year is divisible by 4
     if (year % 4 == 0) {
       is_leap_year = true;
 
-      // To identify whether it is century year or not
       if (year % 100 == 0) {
-        // Checking if year is divisible by 400 therefore century leap year
         if (year % 400 == 0) {
           is_leap_year = true;
         } else {
@@ -20,10 +16,7 @@ public class LeapYearCalculator {
       }
     }
 
-    // We land here when corresponding if fails
-    // If year is not divisible by 4
     else {
-      // Flag dealing with Non leap-year
       is_leap_year = false;
     }
 
